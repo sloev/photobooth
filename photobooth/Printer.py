bf365d05cda3f1f29a1cd93f3ad0b2eb9c121579
sloev <johannesgj@gmail.com>
2014-04-13 22:20:40 +0200

    ds

--- photobooth/Printer.py
+++ photobooth/Printer.py
@@ -370,42 +370,14 @@ if __name__ == '__main__':
 
     print "Testing printer on port %s" % serialport
     p = ThermalPrinter(serialport=serialport)
-    p.print_text("\nHello maailma. How's it going?\n")
-    p.print_text("Part of this ")
-    p.bold_on()
-    p.print_text("line is bold\n")
-    p.bold_off()
-    p.print_text("Part of this ")
-    p.font_b_on()
-    p.print_text("line is fontB\n")
-    p.font_b_off()
-    p.justify("R")
-    p.print_text("right justified\n")
-    p.justify("C")
-    p.print_text("centered\n")
-    p.justify() # justify("L") works too
-    p.print_text("left justified\n")
-    p.upsidedown_on()
-    p.print_text("upside down\n")
-    p.upsidedown_off()
 
-    markup = """bl bold left
-ur underline right
-fc font b centred (next line blank)
-nl
-il inverse left
-"""
-    p.print_markup(markup)
 
     # runtime dependency on Python Imaging Library
     image = Image.open("test.jpg")
     #data = list(i.getdata())
     #w, h = i.size
     p.print_bitmap(image)
-    p.linefeed()
-    p.justify("C")
-    p.barcode_chr("2")
-    p.barcode("014633098808")
+
     p.linefeed()
     p.linefeed()
     p.linefeed()
