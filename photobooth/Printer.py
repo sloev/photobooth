46580e65c2180a64a0da0e47ccdd9fc8ac7744e7
sloev <johannesgj@gmail.com>
2014-04-13 14:33:06 +0200

    sdfv

--- photobooth/Printer.py
+++ photobooth/Printer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
 from Modules.ThermalPrinter import Adafruit_Thermal
-import Image
+import Image,time
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 
+printer.wake()
 
 # Barcode examples
+printer.setDefault() # Restore printer to defaults
+
 printer.feed(1)
 image=Image.open("test.jpg")
 bbox=image.getbbox()
@@ -14,6 +17,9 @@ if bbox[2] > 384:
     image=image.crop(((bbox[2]/2)-(bbox[3]/2),0,(bbox[2]/2)+(bbox[3]/2),bbox[3]))
     image=image.resize((384,384))
 for i in range(4):
+    printer.sleep()      # Tell printer to sleep
+    time.sleep(1)
+    printer.wake()
     printer.printImage(image, True)
 
 #printer.printBitmap(adalogo.width, adalogo.height, adalogo.data)
