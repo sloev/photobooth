c227023ed846e5d45857b36515b426771810fbe6
sloev <johannesgj@gmail.com>
2014-04-13 14:30:13 +0200

    adfs

--- photobooth/Printer.py
+++ photobooth/Printer.py
@@ -13,12 +13,12 @@ bbox=image.getbbox()
 if bbox[2] > 384:
     image=image.crop(((bbox[2]/2)-(bbox[3]/2),0,(bbox[2]/2)+(bbox[3]/2),bbox[3]))
     image=image.resize((384,384))
-    
-printer.printImage(image, True)
+for i in range(4):
+    printer.printImage(image, True)
 
 #printer.printBitmap(adalogo.width, adalogo.height, adalogo.data)
 
-printer.feed(1)
+    printer.feed(1)
 
 printer.sleep()      # Tell printer to sleep
 printer.wake()       # Call wake() before printing again, even if reset
