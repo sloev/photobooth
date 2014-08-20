3be0f5d8205ff8a2969c3a59561f5e98b26fef4d
sloev <johannesgj@gmail.com>
2014-08-20 11:46:25 +0200

    instagramming 4

--- photobooth/Photobooth.py
+++ photobooth/Photobooth.py
@@ -106,7 +106,7 @@ def main():
                 time.sleep(0.2)
                 if(GPIO.input(4)==0):
                     photobooth.startShoot()
-             while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
                 c = sys.stdin.readline()
                 c=c[0:1]
                 if(c=='s'):
